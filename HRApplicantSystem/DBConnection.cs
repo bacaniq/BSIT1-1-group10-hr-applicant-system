634c21432d8710f314a75a72a0cf86f203cb25d0
dennisanzu09 <viernezadennis7@gmail.com>
2026-06-07 00:46:42 +0800

    Add project files.

---
+++ HRApplicantSystem/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace HRApplicantSystem
+{
+    public class DBConnection
+    {
+        private static string connectionString =
+            "Server=localhost;Database=HRApplicantDB;Uid=root;Pwd=root1234;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
---
+++ HRApplicantSystem/Dashboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRApplicantSystem
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var conn = DBConnection.GetConnection();
+                conn.Open();
+                MessageBox.Show("Connected to database successfully!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
