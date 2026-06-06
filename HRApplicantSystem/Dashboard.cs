8320e7cba01f6b43120ef3f32e883f6c614f983d
dennisanzu09 <viernezadennis7@gmail.com>
2026-06-07 04:16:26 +0800

    Added Applicant Dashboard page

--- HRApplicantSystem/Dashboard.cs
+++ HRApplicantSystem/Dashboard.cs
@@ -1,35 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace HRApplicantSystem
 {
     public partial class Dashboard : Form
     {
+        public static int ApplicantID = 1;
+
         public Dashboard()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            LoadDashboard();
+        }
+
+        private void LoadDashboard()
         {
             try
             {
                 var conn = DBConnection.GetConnection();
                 conn.Open();
-                MessageBox.Show("Connected to database successfully!");
+
+                string nameQuery = "SELECT FirstName, LastName FROM Applicants WHERE ApplicantID = " + ApplicantID;
+                MySqlCommand nameCmd = new MySqlCommand(nameQuery, conn);
+                MySqlDataReader nameReader = nameCmd.ExecuteReader();
+                if (nameReader.Read())
+                {
+                    lblWelcome.Text = "Welcome, " + nameReader["FirstName"] + "!";
+                }
+                nameReader.Close();
+
+                string statusQuery = "SELECT Status FROM Applications WHERE ApplicantID = " + ApplicantID;
+                MySqlCommand statusCmd = new MySqlCommand(statusQuery, conn);
+                MySqlDataReader statusReader = statusCmd.ExecuteReader();
+                if (statusReader.Read())
+                {
+                    lblStatusValue.Text = statusReader["Status"].ToString();
+                }
+                else
+                {
+                    lblStatusValue.Text = "No application yet";
+                }
+                statusReader.Close();
+
+                string docQuery = "SELECT COUNT(*) FROM ApplicantDocuments WHERE ApplicantID = " + ApplicantID + " AND Status = 'Missing'";
+                MySqlCommand docCmd = new MySqlCommand(docQuery, conn);
+                int missingCount = Convert.ToInt32(docCmd.ExecuteScalar());
+                lblMissingValue.Text = missingCount.ToString();
+
+                string intQuery = "SELECT i.ScheduledDate, i.Mode FROM InterviewSchedules i " +
+                                  "JOIN Applications a ON i.ApplicationID = a.ApplicationID " +
+                                  "WHERE a.ApplicantID = " + ApplicantID;
+                MySqlCommand intCmd = new MySqlCommand(intQuery, conn);
+                MySqlDataReader intReader = intCmd.ExecuteReader();
+                if (intReader.Read())
+                {
+                    lblInterviewValue.Text = intReader["ScheduledDate"] + " - " + intReader["Mode"];
+                }
+                else
+                {
+                    lblInterviewValue.Text = "No interview scheduled";
+                }
+                intReader.Close();
+
                 conn.Close();
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message);
             }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My Profile - Coming Soon!");
+        }
+
+        private void btnJobs_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Job Vacancies - Coming Soon!");
+        }
+
+        private void btnApplication_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My Application - Coming Soon!");
+        }
+
+        private void btnDocuments_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My Documents - Coming Soon!");
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Application Status - Coming Soon!");
         }
     }
 }
--- HRApplicantSystem/Form1.Designer.cs
+++ HRApplicantSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HRApplicantSystem
 {
-    partial class Dashboard
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,37 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblWelcome = new System.Windows.Forms.Label();
             this.lblStatus = new System.Windows.Forms.Label();
             this.lblStatusValue = new System.Windows.Forms.Label();
-            this.lblMissing = new System.Windows.Forms.Label();
-            this.lblMissingValue = new System.Windows.Forms.Label();
+            this.lblMissingDocs = new System.Windows.Forms.Label();
             this.lblInterview = new System.Windows.Forms.Label();
             this.lblInterviewValue = new System.Windows.Forms.Label();
+            this.lblMissingValue = new System.Windows.Forms.Label();
             this.btnProfile = new System.Windows.Forms.Button();
             this.btnJobs = new System.Windows.Forms.Button();
             this.btnApplication = new System.Windows.Forms.Button();
             this.btnDocuments = new System.Windows.Forms.Button();
             this.btnStatus = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblWelcome
-            // 
-            this.lblWelcome.AutoSize = true;
-            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWelcome.ForeColor = System.Drawing.Color.SteelBlue;
-            this.lblWelcome.Location = new System.Drawing.Point(29, 27);
-            this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(154, 36);
-            this.lblWelcome.TabIndex = 0;
-            this.lblWelcome.Text = "Welcome!";
             // 
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatus.Location = new System.Drawing.Point(165, 89);
+            this.lblStatus.Location = new System.Drawing.Point(66, 120);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(168, 20);
             this.lblStatus.TabIndex = 1;
@@ -67,60 +56,64 @@
             // lblStatusValue
             // 
             this.lblStatusValue.AutoSize = true;
+            this.lblStatusValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStatusValue.ForeColor = System.Drawing.Color.Gray;
-            this.lblStatusValue.Location = new System.Drawing.Point(482, 89);
+            this.lblStatusValue.Location = new System.Drawing.Point(379, 120);
             this.lblStatusValue.Name = "lblStatusValue";
-            this.lblStatusValue.Size = new System.Drawing.Size(115, 16);
+            this.lblStatusValue.Size = new System.Drawing.Size(142, 20);
             this.lblStatusValue.TabIndex = 2;
             this.lblStatusValue.Text = "No application yet";
             // 
-            // lblMissing
+            // lblMissingDocs
             // 
-            this.lblMissing.AutoSize = true;
-            this.lblMissing.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMissing.Location = new System.Drawing.Point(165, 131);
-            this.lblMissing.Name = "lblMissing";
-            this.lblMissing.Size = new System.Drawing.Size(181, 20);
-            this.lblMissing.TabIndex = 3;
-            this.lblMissing.Text = "Missing Documents:";
-            // 
-            // lblMissingValue
-            // 
-            this.lblMissingValue.AutoSize = true;
-            this.lblMissingValue.ForeColor = System.Drawing.Color.Red;
-            this.lblMissingValue.Location = new System.Drawing.Point(529, 131);
-            this.lblMissingValue.Name = "lblMissingValue";
-            this.lblMissingValue.Size = new System.Drawing.Size(11, 16);
-            this.lblMissingValue.TabIndex = 4;
-            this.lblMissingValue.Text = "-";
+            this.lblMissingDocs.AutoSize = true;
+            this.lblMissingDocs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMissingDocs.Location = new System.Drawing.Point(66, 173);
+            this.lblMissingDocs.Name = "lblMissingDocs";
+            this.lblMissingDocs.Size = new System.Drawing.Size(181, 20);
+            this.lblMissingDocs.TabIndex = 3;
+            this.lblMissingDocs.Text = "Missing Documents:";
             // 
             // lblInterview
             // 
             this.lblInterview.AutoSize = true;
-            this.lblInterview.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInterview.Location = new System.Drawing.Point(165, 177);
+            this.lblInterview.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInterview.Location = new System.Drawing.Point(66, 224);
             this.lblInterview.Name = "lblInterview";
-            this.lblInterview.Size = new System.Drawing.Size(154, 20);
-            this.lblInterview.TabIndex = 5;
+            this.lblInterview.Size = new System.Drawing.Size(173, 20);
+            this.lblInterview.TabIndex = 4;
             this.lblInterview.Text = "Interview Schedule:";
+            this.lblInterview.Click += new System.EventHandler(this.lblInterview_Click_1);
             // 
             // lblInterviewValue
             // 
             this.lblInterviewValue.AutoSize = true;
+            this.lblInterviewValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInterviewValue.ForeColor = System.Drawing.Color.Gray;
-            this.lblInterviewValue.Location = new System.Drawing.Point(478, 177);
+            this.lblInterviewValue.Location = new System.Drawing.Point(379, 224);
             this.lblInterviewValue.Name = "lblInterviewValue";
-            this.lblInterviewValue.Size = new System.Drawing.Size(146, 16);
-            this.lblInterviewValue.TabIndex = 6;
+            this.lblInterviewValue.Size = new System.Drawing.Size(182, 20);
+            this.lblInterviewValue.TabIndex = 5;
             this.lblInterviewValue.Text = "No interview scheduled";
+            // 
+            // lblMissingValue
+            // 
+            this.lblMissingValue.AutoSize = true;
+            this.lblMissingValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMissingValue.ForeColor = System.Drawing.Color.Red;
+            this.lblMissingValue.Location = new System.Drawing.Point(442, 173);
+            this.lblMissingValue.Name = "lblMissingValue";
+            this.lblMissingValue.Size = new System.Drawing.Size(18, 20);
+            this.lblMissingValue.TabIndex = 6;
+            this.lblMissingValue.Text = "0";
             // 
             // btnProfile
             // 
             this.btnProfile.BackColor = System.Drawing.Color.SteelBlue;
             this.btnProfile.ForeColor = System.Drawing.Color.White;
-            this.btnProfile.Location = new System.Drawing.Point(105, 242);
+            this.btnProfile.Location = new System.Drawing.Point(70, 295);
             this.btnProfile.Name = "btnProfile";
-            this.btnProfile.Size = new System.Drawing.Size(175, 61);
+            this.btnProfile.Size = new System.Drawing.Size(155, 55);
             this.btnProfile.TabIndex = 7;
             this.btnProfile.Text = "My Profile";
             this.btnProfile.UseVisualStyleBackColor = false;
@@ -129,9 +122,9 @@
             // 
             this.btnJobs.BackColor = System.Drawing.Color.SteelBlue;
             this.btnJobs.ForeColor = System.Drawing.Color.White;
-            this.btnJobs.Location = new System.Drawing.Point(449, 242);
+            this.btnJobs.Location = new System.Drawing.Point(406, 288);
             this.btnJobs.Name = "btnJobs";
-            this.btnJobs.Size = new System.Drawing.Size(175, 61);
+            this.btnJobs.Size = new System.Drawing.Size(155, 62);
             this.btnJobs.TabIndex = 8;
             this.btnJobs.Text = "View Job Vacancies";
             this.btnJobs.UseVisualStyleBackColor = false;
@@ -140,9 +133,9 @@
             // 
             this.btnApplication.BackColor = System.Drawing.Color.SteelBlue;
             this.btnApplication.ForeColor = System.Drawing.Color.White;
-            this.btnApplication.Location = new System.Drawing.Point(105, 376);
+            this.btnApplication.Location = new System.Drawing.Point(70, 383);
             this.btnApplication.Name = "btnApplication";
-            this.btnApplication.Size = new System.Drawing.Size(175, 61);
+            this.btnApplication.Size = new System.Drawing.Size(155, 55);
             this.btnApplication.TabIndex = 9;
             this.btnApplication.Text = "My Application";
             this.btnApplication.UseVisualStyleBackColor = false;
@@ -151,9 +144,9 @@
             // 
             this.btnDocuments.BackColor = System.Drawing.Color.SteelBlue;
             this.btnDocuments.ForeColor = System.Drawing.Color.White;
-            this.btnDocuments.Location = new System.Drawing.Point(449, 309);
+            this.btnDocuments.Location = new System.Drawing.Point(406, 383);
             this.btnDocuments.Name = "btnDocuments";
-            this.btnDocuments.Size = new System.Drawing.Size(175, 61);
+            this.btnDocuments.Size = new System.Drawing.Size(155, 55);
             this.btnDocuments.TabIndex = 10;
             this.btnDocuments.Text = "My Documents";
             this.btnDocuments.UseVisualStyleBackColor = false;
@@ -162,9 +155,9 @@
             // 
             this.btnStatus.BackColor = System.Drawing.Color.SteelBlue;
             this.btnStatus.ForeColor = System.Drawing.Color.White;
-            this.btnStatus.Location = new System.Drawing.Point(105, 309);
+            this.btnStatus.Location = new System.Drawing.Point(70, 463);
             this.btnStatus.Name = "btnStatus";
-            this.btnStatus.Size = new System.Drawing.Size(175, 61);
+            this.btnStatus.Size = new System.Drawing.Size(155, 55);
             this.btnStatus.TabIndex = 11;
             this.btnStatus.Text = "Application Status";
             this.btnStatus.UseVisualStyleBackColor = false;
@@ -173,53 +166,66 @@
             // 
             this.btnLogout.BackColor = System.Drawing.Color.Red;
             this.btnLogout.ForeColor = System.Drawing.Color.White;
-            this.btnLogout.Location = new System.Drawing.Point(663, 391);
+            this.btnLogout.Location = new System.Drawing.Point(549, 486);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(137, 46);
+            this.btnLogout.Size = new System.Drawing.Size(102, 46);
             this.btnLogout.TabIndex = 12;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
             // 
-            // Dashboard
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblWelcome.Location = new System.Drawing.Point(64, 34);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(154, 36);
+            this.lblWelcome.TabIndex = 13;
+            this.lblWelcome.Text = "Welcome!";
+            // 
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 466);
+            this.ClientSize = new System.Drawing.Size(676, 553);
+            this.Controls.Add(this.lblWelcome);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnStatus);
             this.Controls.Add(this.btnDocuments);
             this.Controls.Add(this.btnApplication);
             this.Controls.Add(this.btnJobs);
             this.Controls.Add(this.btnProfile);
+            this.Controls.Add(this.lblMissingValue);
             this.Controls.Add(this.lblInterviewValue);
             this.Controls.Add(this.lblInterview);
-            this.Controls.Add(this.lblMissingValue);
-            this.Controls.Add(this.lblMissing);
+            this.Controls.Add(this.lblMissingDocs);
             this.Controls.Add(this.lblStatusValue);
             this.Controls.Add(this.lblStatus);
-            this.Controls.Add(this.lblWelcome);
-            this.Name = "Dashboard";
-            this.Text = "Dashboard";
-            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Applicant Dashboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblWelcome;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Label lblStatusValue;
-        private System.Windows.Forms.Label lblMissing;
-        private System.Windows.Forms.Label lblMissingValue;
+        private System.Windows.Forms.Label lblMissingDocs;
         private System.Windows.Forms.Label lblInterview;
         private System.Windows.Forms.Label lblInterviewValue;
+        private System.Windows.Forms.Label lblMissingValue;
         private System.Windows.Forms.Button btnProfile;
         private System.Windows.Forms.Button btnJobs;
         private System.Windows.Forms.Button btnApplication;
         private System.Windows.Forms.Button btnDocuments;
         private System.Windows.Forms.Button btnStatus;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblWelcome;
     }
 }
