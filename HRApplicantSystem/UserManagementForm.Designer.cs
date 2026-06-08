b58808180ae81856efca803421b464927f75a05d
VickyB <bacaniq216@gmail.com>
2026-06-08 15:59:08 +0800

    Add UserManagementForm
    Includes Add and Delete User Function

---
+++ HRApplicantSystem/UserManagementForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace HRApplicantSystem
+{
+    partial class UserManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listView1 = new ListView();
+            FullName = new ColumnHeader();
+            Email = new ColumnHeader();
+            Role = new ColumnHeader();
+            Status = new ColumnHeader();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(279, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(242, 37);
+            label1.TabIndex = 0;
+            label1.Text = " User Management";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { FullName, Email, Role, Status });
+            listView1.Location = new Point(12, 49);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 162);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // FullName
+            // 
+            FullName.Text = "FullName";
+            // 
+            // Email
+            // 
+            Email.Text = "Email";
+            // 
+            // Role
+            // 
+            Role.Text = "Role";
+            // 
+            // Status
+            // 
+            Status.Text = "Status";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(202, 243);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(195, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(474, 243);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(195, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(202, 284);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(195, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Admin", "HR Manager", "HR Staff" });
+            comboBox1.Location = new Point(474, 287);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(195, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(275, 360);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 43);
+            button1.TabIndex = 6;
+            button1.Text = "Add User";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(415, 360);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 45);
+            button2.TabIndex = 7;
+            button2.Text = "Delete User";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(132, 246);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Full Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(429, 246);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(132, 290);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Password:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(429, 290);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Role:";
+            // 
+            // UserManagementForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Controls.Add(label1);
+            Name = "UserManagementForm";
+            Text = "UserManagementForm";
+            Load += UserManagementForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListView listView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ColumnHeader FullName;
+        private ColumnHeader Email;
+        private ColumnHeader Role;
+        private ColumnHeader Status;
+    }
+}
