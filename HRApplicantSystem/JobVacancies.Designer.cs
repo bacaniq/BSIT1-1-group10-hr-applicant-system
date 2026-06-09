20b648886fc47d861ba9391303fb5675c3a45390
VickyB <bacaniq216@gmail.com>
2026-06-10 00:15:08 +0800

    Minor debug

--- HRApplicantSystem/JobVacancies.Designer.cs
+++ HRApplicantSystem/JobVacancies.Designer.cs
@@ -122,7 +122,7 @@
             this.btnBack.TabIndex = 6;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click_1);
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // lblTitle
             // 
