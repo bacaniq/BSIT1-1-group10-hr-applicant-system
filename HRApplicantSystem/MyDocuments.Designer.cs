d9d0021af1a143d01d9d10b913ba82ff6a43d077
dennisanzu09 <dennisvierneza7@gmail.com>
2026-06-17 01:29:12 +0800

    Add files via upload

---
+++ HRApplicantSystem/MyDocuments.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace HRApplicantSystem
+{
+    partial class MyDocuments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.listViewDocs = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblDocType = new System.Windows.Forms.Label();
+            this.cmbDocType = new System.Windows.Forms.ComboBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnViewFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitle.Location = new System.Drawing.Point(283, 26);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(207, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "My Documents";
+            // 
+            // listViewDocs
+            // 
+            this.listViewDocs.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewDocs.FullRowSelect = true;
+            this.listViewDocs.GridLines = true;
+            this.listViewDocs.HideSelection = false;
+            this.listViewDocs.Location = new System.Drawing.Point(128, 99);
+            this.listViewDocs.MultiSelect = false;
+            this.listViewDocs.Name = "listViewDocs";
+            this.listViewDocs.Size = new System.Drawing.Size(505, 97);
+            this.listViewDocs.TabIndex = 1;
+            this.listViewDocs.UseCompatibleStateImageBehavior = false;
+            this.listViewDocs.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Document Type";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File Name";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Remarks";
+            this.columnHeader4.Width = 150;
+            // 
+            // lblDocType
+            // 
+            this.lblDocType.AutoSize = true;
+            this.lblDocType.Location = new System.Drawing.Point(147, 253);
+            this.lblDocType.Name = "lblDocType";
+            this.lblDocType.Size = new System.Drawing.Size(106, 16);
+            this.lblDocType.TabIndex = 2;
+            this.lblDocType.Text = "Document Type:";
+            // 
+            // cmbDocType
+            // 
+            this.cmbDocType.FormattingEnabled = true;
+            this.cmbDocType.Items.AddRange(new object[] {
+            "Resume",
+            "ID",
+            "Transcript",
+            "Certificate",
+            "Birth Certificate",
+            "Diploma",
+            "Medical Clearance",
+            "Police Clearance"});
+            this.cmbDocType.Location = new System.Drawing.Point(289, 253);
+            this.cmbDocType.Name = "cmbDocType";
+            this.cmbDocType.Size = new System.Drawing.Size(121, 24);
+            this.cmbDocType.TabIndex = 3;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(181, 294);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(72, 16);
+            this.lblFileName.TabIndex = 4;
+            this.lblFileName.Text = "File Name:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(289, 291);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(121, 22);
+            this.txtFileName.TabIndex = 5;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.Green;
+            this.btnUpload.ForeColor = System.Drawing.Color.White;
+            this.btnUpload.Location = new System.Drawing.Point(35, 366);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(135, 44);
+            this.btnUpload.TabIndex = 6;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnReplace.ForeColor = System.Drawing.Color.White;
+            this.btnReplace.Location = new System.Drawing.Point(215, 366);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(135, 44);
+            this.btnReplace.TabIndex = 7;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = false;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Gray;
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(619, 366);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(135, 44);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(454, 285);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(149, 34);
+            this.btnBrowse.TabIndex = 9;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnViewFile
+            // 
+            this.btnViewFile.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnViewFile.ForeColor = System.Drawing.Color.White;
+            this.btnViewFile.Location = new System.Drawing.Point(388, 366);
+            this.btnViewFile.Name = "btnViewFile";
+            this.btnViewFile.Size = new System.Drawing.Size(126, 43);
+            this.btnViewFile.TabIndex = 10;
+            this.btnViewFile.Text = "View File";
+            this.btnViewFile.UseVisualStyleBackColor = false;
+            this.btnViewFile.Click += new System.EventHandler(this.btnViewFile_Click);
+            // 
+            // MyDocuments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.btnViewFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.cmbDocType);
+            this.Controls.Add(this.lblDocType);
+            this.Controls.Add(this.listViewDocs);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MyDocuments";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Documents";
+            this.Load += new System.EventHandler(this.MyDocuments_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ListView listViewDocs;
+        private System.Windows.Forms.Label lblDocType;
+        private System.Windows.Forms.ComboBox cmbDocType;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnViewFile;
+    }
+}
